e5cc53154dc7c9bf94f3a3e421dcc6b254c7c3ca
bastyha <szentesidominik@gmail.com>
2022-07-14 20:41:54 +0200

    level 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("localhost:8080").unwrap(); //creating a TcpListener and biding it to a port
@@ -16,17 +17,24 @@ fn main() {
 fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 1024]; //suitable buffer for now
     //reading request from the TCP stream
-    stream.read(&mut buffer).unwrap(); //get the data (as bytes) from the TcpStream and putting them in a buffer
+    stream.read(&mut buffer).unwrap(); //get the request (as bytes) from the TcpStream and putting them in a buffer
     
     let get = b"GET /healthz HTTP/1.1\r\n"; //checking if request is for healthz
+    let accounts = b"GET /accounts HTTP/1.1\r\n";
 
-    if buffer.starts_with(get){
-
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
-        
-        //converting the response to bytes and sending it
-        stream.write(response.as_bytes()).unwrap();
-    
-        stream.flush().unwrap();//prevents the program from continuing whil the bytes from the response are being sent 
+    let mut response = String::new();
+    let status_line = "HTTP/1.1 200 OK\r\n";
+    if buffer.starts_with(get){ //checking what address
+        response = String::from(status_line); // when /healthz sending only the status line back
+    }else if buffer.starts_with(accounts){
+        let content = fs::read_to_string("accounts.json").unwrap(); //reading data from file
+        //creating a response from the contents of the file, and the status line
+        response = format!("{}Content-Length: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content);
     }
+    stream.write(response.as_bytes()).unwrap();//sending response
+    
+    stream.flush().unwrap();//prevents the program from continuing while the bytes from the response are being sent 
 }
