0ea5b383458af79603a5ff4b812c345f46134fe2
bastyha <szentesidominik@gmail.com>
2022-07-15 11:52:25 +0200

    Level 2

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,56 @@ fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap(); //get the request (as bytes) from the TcpStream and putting them in a buffer
     
     let get = b"GET /healthz HTTP/1.1\r\n"; //checking if request is for healthz
-    let accounts = b"GET /accounts HTTP/1.1\r\n";
+    
+    let mut status_line = String::new();
+    let mut content = String::new();
 
-    let mut response = String::new();
-    let status_line = "HTTP/1.1 200 OK\r\n";
     if buffer.starts_with(get){ //checking what address
-        response = String::from(status_line); // when /healthz sending only the status line back
-    }else if buffer.starts_with(accounts){
-        let content = fs::read_to_string("accounts.json").unwrap(); //reading data from file
-        //creating a response from the contents of the file, and the status line
-        response = format!("{}Content-Length: {}\r\n\r\n{}",
-        status_line,
-        content.len(),
-        content);
+        status_line = "HTTP/1.1 200 OK\r\n".to_string();
+        content = "".to_string();
+    }else {
+        let request = String::from_utf8_lossy(&buffer[..]);
+        let getting_account_id = request.split_whitespace().collect::<Vec<_>>();
+        let json_file = fs::read_to_string("accounts.json").unwrap(); //reading data from file
+        
+
+        match getting_account_id[1][1..].find('/'){
+            None=>{
+            //creating a response from the contents of the file, and the status line
+            content = json_file;
+            status_line = "HTTP/1.1 200 OK\r\n".to_string();
+            }
+            Some(id)=>{
+
+                let person_id = format!("\"id\": \"{}\"", getting_account_id[1][(id+2)..].to_string());
+                
+                for line in json_file.lines(){
+                    match line.find(person_id.as_str()){
+                        Some(_)=>{
+                           status_line = "HTTP/1.1 200 OK\r\n".to_string();
+                           content = line.trim_end().to_string();
+                           break;
+                        }
+                        None=>{
+                            status_line ="HTTP/1.1 404 Not Found\r\n".to_string();
+                            content="".to_string();
+                        }
+                    }
+                }
+            
+            }
+        };
     }
+
+    let response =             
+    format!("{}Content-Length: {}\r\n\r\n{}",
+    status_line,
+    content.len(),
+    content);
+
     stream.write(response.as_bytes()).unwrap();//sending response
     
     stream.flush().unwrap();//prevents the program from continuing while the bytes from the response are being sent 
 }
+
+
